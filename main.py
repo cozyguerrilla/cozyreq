d4dcba8d5c02d8b864ee4e089e2d47aec6995691
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-11-26 10:08:41 +0100

    Clean up project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from agent!")
+    print("Hello from cozyreq!")
 
 
 if __name__ == "__main__":
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "vitest";
-import { render } from "@solidjs/testing-library";
-import App from "./App";
-
-test("increments value", async () => {
-  const { getByRole } = render(() => <App />);
-  const headline = getByRole("heading");
-  expect(headline).toHaveTextContent("Welcome to Tauri + Solid");
-});
