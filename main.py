534f20d64c8bc47ed29a3758472dcda3391550e7
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-12-01 22:09:29 +0100

    Add FastAPI example

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from cozyreq!")
+from uuid import UUID
+
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: UUID, q: str | None = None):
+    return {"item_id": item_id, "q": q}
